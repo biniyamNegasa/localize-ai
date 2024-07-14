f2e0fe6f264d0501cc74ef58f154651448216673
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-07-14 21:09:06 +0300

    Added the main part with templet example and also implmented some examples as prompt

---
+++ front-end-part/src/Components/Examples/Example.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Example.css';
+import { AiOutlineSun } from "react-icons/ai";
+import { useNavigate } from 'react-router-dom';
+
+
+const Example = () => {
+  const navigate = useNavigate();
+
+  const handleSentenceClick = () => {
+    navigate("/main");
+  };
+
+  const sentences = [
+    "Can you explain the concept of object-oriented programming?",
+    "What are some effective time management techniques?",
+    "How do I write a thesis statement?"
+  ];
+
+  return (
+    <div className="example-container">
+      <div className="example-icon"><AiOutlineSun /></div>
+
+        <p className="displayer-example-name">Examples</p>
+
+        <p className="paragraph">
+          {sentences.map((sentence, index) => (
+            <div
+              key={index}
+              onClick={() => handleSentenceClick()}
+              className="clickable-sentence"
+            >
+              {sentence + ''}<br/>
+            </div>
+          ))}
+        </p>
+    
+    </div>
+  );
+};
+
+export default Example;
